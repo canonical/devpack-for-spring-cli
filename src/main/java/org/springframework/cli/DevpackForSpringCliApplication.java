75ae94e297fb673cc62cb0384cc46e9e80fc1e95
Vladimir Petko <vladimir.petko@canonical.com>
2026-03-27 13:20:51 +1300

    fix: implement the banner in the Spring way (#128)
    The banner can be displayed by the application itself.
    
    This commit reverts custom prompt provider,
    sets up Spring Banner and ensures that logging
    is configured using Spring Appender (disable custom
    logback provider).

--- src/main/java/org/springframework/cli/DevpackForSpringCliApplication.java
+++ src/main/java/org/springframework/cli/DevpackForSpringCliApplication.java
@@ -18,7 +18,9 @@ package org.springframework.cli;
 
 import java.util.Arrays;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cli.config.SpringCliRuntimeHints;
@@ -35,23 +37,35 @@ import org.springframework.shell.command.annotation.CommandScan;
 @CommandScan
 public class DevpackForSpringCliApplication {
 
+	private static final String BANNER_TEXT = AnsiOutput.encode(AnsiColor.BRIGHT_YELLOW)
+			+ "DEVPACK-FOR-SPRING INTERACTIVE MODE" + AnsiOutput.encode(AnsiColor.DEFAULT) + "\n" + "\ttype "
+			+ AnsiOutput.encode(AnsiColor.BRIGHT_GREEN) + "\"help\"" + AnsiOutput.encode(AnsiColor.DEFAULT)
+			+ " to see the list of available commands";
+
 	public static void main(String[] args) {
 		System.setProperty("java.awt.headless", Boolean.toString(true));
 		boolean debug = false;
-		if (args.length > 1) {
+		if (args.length >= 1) {
 			debug = "--debug".equals(args[0]);
 			if (debug) {
 				args = Arrays.stream(args).skip(1).toArray(String[]::new);
 			}
 		}
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(DevpackForSpringCliApplication.class)
+			.properties("spring.config.name=devpack-for-springcliapp")
+			.properties("spring.config.location=classpath:/devpack-for-springcliapp.yml")
+			.properties(String.format("app.debug=%b", debug))
+			.properties("logging.level.org.springframework.boot.SpringApplication=" + (debug ? "DEBUG" : "OFF"))
+			.properties("logging.level.root=" + (debug ? "DEBUG" : "ERROR"));
+		if (args.length == 0) {
+			builder.bannerMode(Banner.Mode.CONSOLE);
+			builder.banner((environment, sourceClass, out) -> out.println(BANNER_TEXT));
+		}
+		else {
+			builder.bannerMode(Banner.Mode.OFF);
+		}
 		try {
-			SpringApplication app = new SpringApplicationBuilder(DevpackForSpringCliApplication.class)
-				.properties("spring.config.name=devpack-for-springcliap")
-				.properties("spring.config.location=classpath:/devpack-for-springcliapp.yml")
-				.properties(String.format("app.debug=%b", debug))
-				.properties("logging.level.org.springframework.boot.SpringApplication=" + (debug ? "DEBUG" : "OFF"))
-				.build();
-			app.run(args);
+			builder.build().run(args);
 		}
 		catch (Throwable tx) {
 			if (debug) {
