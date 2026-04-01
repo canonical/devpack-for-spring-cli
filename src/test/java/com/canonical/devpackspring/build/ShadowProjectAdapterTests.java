093f4284be7472bded80ff2e95313fe0feee05b9
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 08:53:02 +1300

    test: add move project file test
    - add project file test
    - remove context runner (its not needed)

--- src/test/java/com/canonical/devpackspring/build/ShadowProjectAdapterTests.java
+++ src/test/java/com/canonical/devpackspring/build/ShadowProjectAdapterTests.java
@@ -16,7 +16,14 @@
 
 package com.canonical.devpackspring.build;
 
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -33,83 +40,149 @@ public class ShadowProjectAdapterTests {
 		.withUserConfiguration(MockConfigurations.MockBaseConfig.class);
 
 	@Test
-	void testCloneProject(final @TempDir Path workingDir) {
+	void testCloneProject(final @TempDir Path workingDir) throws IOException {
 		Path projectPath = Path.of("test-data").resolve("projects").resolve("gradle-kotlin");
 		IntegrationTestSupport.installInWorkingDirectory(projectPath, workingDir);
-		contextRunner.withUserConfiguration(MockConfigurations.MockUserConfig.class).run(context -> {
-			Path clonedPath;
-			ShadowProjectAdapter adapter = new ShadowProjectAdapter(workingDir, new PluginResource[0]);
-			clonedPath = adapter.getProjectPath();
-			// The project should be symlinked and provide subdirectories with files
-			assertThat(clonedPath).exists();
-			assertThat(clonedPath.resolve("gradle")).exists();
-			assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
-			// root files should be present
-			assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
-			// settings is a symbolic link
-			assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
-			// build is a regular file
-			assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
+		Path clonedPath;
+		ShadowProjectAdapter adapter = new ShadowProjectAdapter(workingDir, new PluginResource[0]);
+		clonedPath = adapter.getProjectPath();
+		// The project should be symlinked and provide subdirectories with files
+		assertThat(clonedPath).exists();
+		assertThat(clonedPath.resolve("gradle")).exists();
+		assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
+		// root files should be present
+		assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
+		// settings is a symbolic link
+		assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
+		// build is a regular file
+		assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
 
-			// ShadowProjectAdapter deletes all extra content before redoing the symlink
-			// validate that nothing changes
-			adapter = new ShadowProjectAdapter(workingDir, new PluginResource[0]);
-			clonedPath = adapter.getProjectPath();
-			assertThat(clonedPath).exists();
-			assertThat(clonedPath.resolve("gradle")).exists();
-			assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
-			// root files should be present
-			assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
-			// settings is a symbolic link
-			assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
-			// build is a regular file
-			assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
+		// ShadowProjectAdapter deletes all extra content before redoing the symlink
+		// validate that nothing changes
+		adapter = new ShadowProjectAdapter(workingDir, new PluginResource[0]);
+		clonedPath = adapter.getProjectPath();
+		assertThat(clonedPath).exists();
+		assertThat(clonedPath.resolve("gradle")).exists();
+		assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
+		// root files should be present
+		assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
+		// settings is a symbolic link
+		assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
+		// build is a regular file
+		assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
+	}
 
+	private static void moveRecursive(Path source, Path target) throws IOException {
+		Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				// Relativize the path to create the same subfolder structure in the
+				// target
+				Path targetDir = target.resolve(source.relativize(dir));
+				try {
+					Files.createDirectories(targetDir);
+				}
+				catch (FileAlreadyExistsException ex) {
+					// Ignore if the directory already exists
+				}
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				// Move file to the corresponding location in target
+				Path targetFile = target.resolve(source.relativize(file));
+				Files.move(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				// Remove the source directory after its contents are moved
+				if (exc == null) {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				}
+				else {
+					throw exc;
+				}
+			}
 		});
-
 	}
 
 	@Test
-	void testCopyResources(final @TempDir Path workingDir) {
+	void testMoveProject(final @TempDir Path workingDir, final @TempDir Path otherDir) throws IOException {
 		Path projectPath = Path.of("test-data").resolve("projects").resolve("gradle-kotlin");
 		IntegrationTestSupport.installInWorkingDirectory(projectPath, workingDir);
-		contextRunner.withUserConfiguration(MockConfigurations.MockUserConfig.class).run(context -> {
-			PluginResource[] initialResources = new PluginResource[] { new PluginResource("res1.txt", "initial"),
-					new PluginResource("dir1/res2.txt", "old content") };
-			ShadowProjectAdapter adapter1 = new ShadowProjectAdapter(workingDir, initialResources);
-			Path clonedPath = adapter1.getProjectPath();
-			assertThat(clonedPath.resolve("res1.txt")).hasContent("initial");
-			assertThat(clonedPath.resolve("dir1/res2.txt")).hasContent("old content");
-			assertThat(clonedPath.resolve("gradle")).exists();
+		Path clonedPath;
+		ShadowProjectAdapter adapter = new ShadowProjectAdapter(workingDir, new PluginResource[0]);
+		clonedPath = adapter.getProjectPath();
+		// The project should be symlinked and provide subdirectories with files
+		assertThat(clonedPath).exists();
+		assertThat(clonedPath.resolve("gradle")).exists();
+		assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
+		// root files should be present
+		assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
+		// settings is a symbolic link
+		assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
+		// build is a regular file
+		assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
 
-			PluginResource[] newResources = new PluginResource[] { new PluginResource("dir1/res2.txt", "new content"),
-					new PluginResource("res3.txt", "added") };
+		moveRecursive(workingDir, otherDir);
 
-			new ShadowProjectAdapter(workingDir, newResources);
-
-			assertThat(clonedPath.resolve("res1.txt")).doesNotExist();
-			assertThat(clonedPath.resolve("dir1/res2.txt")).hasContent("new content");
-			assertThat(clonedPath.resolve("res3.txt")).hasContent("added");
-			assertThat(clonedPath.resolve("gradle")).exists();
-		});
+		// ShadowProjectAdapter should correctly handle the move
+		// validate that nothing changes
+		adapter = new ShadowProjectAdapter(otherDir, new PluginResource[0]);
+		clonedPath = adapter.getProjectPath();
+		assertThat(clonedPath).exists();
+		assertThat(clonedPath.resolve("gradle")).exists();
+		assertThat(clonedPath.resolve("gradle/wrapper/gradle-wrapper.properties")).exists();
+		// root files should be present
+		assertThat(clonedPath.resolve("settings.gradle.kts")).exists();
+		// settings is a symbolic link
+		assertThat(clonedPath.resolve("settings.gradle.kts")).isSymbolicLink();
+		// build is a regular file
+		assertThat(clonedPath.resolve("build.gradle.kts")).doesNotExist();
 	}
 
 	@Test
-	void testAllResourcesRemoved(final @TempDir Path workingDir) {
+	void testCopyResources(final @TempDir Path workingDir) throws IOException {
 		Path projectPath = Path.of("test-data").resolve("projects").resolve("gradle-kotlin");
 		IntegrationTestSupport.installInWorkingDirectory(projectPath, workingDir);
-		contextRunner.withUserConfiguration(MockConfigurations.MockUserConfig.class).run(context -> {
-			PluginResource[] initialResources = new PluginResource[] {
-					new PluginResource("to-be-removed.txt", "content") };
-			ShadowProjectAdapter adapter1 = new ShadowProjectAdapter(workingDir, initialResources);
-			Path clonedPath = adapter1.getProjectPath();
-			assertThat(clonedPath.resolve("to-be-removed.txt")).hasContent("content");
+		PluginResource[] initialResources = new PluginResource[] { new PluginResource("res1.txt", "initial"),
+				new PluginResource("dir1/res2.txt", "old content") };
+		ShadowProjectAdapter adapter1 = new ShadowProjectAdapter(workingDir, initialResources);
+		Path clonedPath = adapter1.getProjectPath();
+		assertThat(clonedPath.resolve("res1.txt")).hasContent("initial");
+		assertThat(clonedPath.resolve("dir1/res2.txt")).hasContent("old content");
+		assertThat(clonedPath.resolve("gradle")).exists();
 
-			new ShadowProjectAdapter(workingDir, new PluginResource[0]);
+		PluginResource[] newResources = new PluginResource[] { new PluginResource("dir1/res2.txt", "new content"),
+				new PluginResource("res3.txt", "added") };
 
-			assertThat(clonedPath.resolve("to-be-removed.txt")).doesNotExist();
-			assertThat(clonedPath.resolve("gradle")).exists();
-		});
+		new ShadowProjectAdapter(workingDir, newResources);
+
+		assertThat(clonedPath.resolve("res1.txt")).doesNotExist();
+		assertThat(clonedPath.resolve("dir1/res2.txt")).hasContent("new content");
+		assertThat(clonedPath.resolve("res3.txt")).hasContent("added");
+		assertThat(clonedPath.resolve("gradle")).exists();
+	}
+
+	@Test
+	void testAllResourcesRemoved(final @TempDir Path workingDir) throws IOException {
+		Path projectPath = Path.of("test-data").resolve("projects").resolve("gradle-kotlin");
+		IntegrationTestSupport.installInWorkingDirectory(projectPath, workingDir);
+
+		PluginResource[] initialResources = new PluginResource[] { new PluginResource("to-be-removed.txt", "content") };
+		ShadowProjectAdapter adapter1 = new ShadowProjectAdapter(workingDir, initialResources);
+		Path clonedPath = adapter1.getProjectPath();
+		assertThat(clonedPath.resolve("to-be-removed.txt")).hasContent("content");
+
+		new ShadowProjectAdapter(workingDir, new PluginResource[0]);
+
+		assertThat(clonedPath.resolve("to-be-removed.txt")).doesNotExist();
+		assertThat(clonedPath.resolve("gradle")).exists();
+
 	}
 
 }
