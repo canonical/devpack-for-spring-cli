0540065b76d6196c13557422df96ec851eaebf0a
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 09:58:09 +1300

    chore: remove unused import

--- src/main/java/com/canonical/devpackspring/build/ShadowProjectAdapter.java
+++ src/main/java/com/canonical/devpackspring/build/ShadowProjectAdapter.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 import org.springframework.util.FileSystemUtils;
 
