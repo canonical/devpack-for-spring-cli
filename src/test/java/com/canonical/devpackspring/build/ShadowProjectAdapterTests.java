06e1adeafb7f756a4d8268d11ec7566d42c6a761
Vladimir Petko <vladimir.petko@canonical.com>
2026-04-02 09:42:30 +1300

    fix: checkstyle warnings

--- src/test/java/com/canonical/devpackspring/build/ShadowProjectAdapterTests.java
+++ src/test/java/com/canonical/devpackspring/build/ShadowProjectAdapterTests.java
@@ -17,13 +17,8 @@
 package com.canonical.devpackspring.build;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
