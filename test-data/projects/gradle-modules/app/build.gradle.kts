388b2c05c2df4ee61075055e621712579562b403
Vladimir Petko <vladimir.petko@canonical.com>
2026-07-29 14:32:57 +1200

    chore: use rockcraft plugin 1.2.6

--- test-data/projects/gradle-modules/app/build.gradle.kts
+++ test-data/projects/gradle-modules/app/build.gradle.kts
@@ -7,7 +7,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("io.github.rockcrafters.rockcraft") version "1.2.5"
+    id("io.github.rockcrafters.rockcraft") version "1.2.6"
 }
 
 repositories {
